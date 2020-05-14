c60e8efd41f91efcc546fa82acc24bd8761dbf01
Antoine <42607264+ShibaeO@users.noreply.github.com>
2020-05-14 11:34:12 +0200

    add, pistol, rifles, sniper, knife, stickers
    Riffles & snipers coverage : 100% (some guns not straight)
    Pistol coverage : 90% (duel_berettas model rework, some guns not straight)
    Knifes coverage : 100% (karambit an talon can be improve but working)
    Stickers coverage 100%

--- main.py
+++ main.py
@@ -60,7 +60,45 @@ def crop_knife(mode, img, id):
         cropped = ImageOps.crop(im, border)
         cropped = cropped.transpose(Image.ROTATE_270)
         cropped.save(f"{id}_backside.png", "png")
-#2d3436
+
+def crop_talon(mode, img, id):
+    if mode == "back":
+        img = Image.open(str(img))
+        border = (160, 81, 160, 81)  # left, up, right, bottom
+        cropped = ImageOps.crop(img, border)
+        rotated = cropped.rotate(65)
+        border = (278, 159, 433, 159)  # left, up, right, bottom
+        im = ImageOps.crop(rotated, border)
+
+        pixels = im.load()
+
+        width, height = im.size
+        for x in range(width):
+            for y in range(height):
+                r, g, b = pixels[x, y]
+                if (r, g, b) == (0, 0, 0):
+                    pixels[x, y] = (45, 52, 54)
+
+        im.save(f"{id}_backside.png", "png")
+
+    elif mode == "play":
+        img = Image.open(str(img))
+        border = (160, 81, 160, 81)  # left, up, right, bottom
+        cropped = ImageOps.crop(img, border)
+        test = cropped.rotate(-65)
+        border = (408, 159, 274, 159)  # left, up, right, bottom
+        im = ImageOps.crop(test, border)
+
+        pixels = im.load()
+
+        width, height = im.size
+        for x in range(width):
+            for y in range(height):
+                r, g, b = pixels[x, y]
+                if (r, g, b) == (0, 0, 0):
+                    pixels[x, y] = (45, 52, 54)
+
+        im.save(f"{id}_playside.png", "png")
 
 def final_image(id, data):
 
@@ -96,7 +134,7 @@ def screen(url, id):
     clickx = super_Var_lol["size"]["x"] * 0.80
     clicky = super_Var_lol["size"]["y"] * 0.80
     os.system(f'start {url}')
-    time.sleep(1.5)
+    time.sleep(2.5)
 
     save_path = f"{id}_playside.png"
     ImageGrab.grab().save(save_path)
@@ -108,11 +146,16 @@ def screen(url, id):
     time.sleep(1)
     ImageGrab.grab().save(save_path)
 
-    if "Knife" in weaponType or weaponType == "Bayonet" or weaponType == "Karambit" or weaponType == "Shadow Daggers":
+   # if weaponType == "Bayonet" or weaponType == "Karambit" or weaponType == "Shadow Daggers" or "Gloves" in weaponType or "Knife" in weaponType:
+    #    save_path = f"{id}_playside.png"
+   #     crop_knife("play", save_path, id)
+    #    save_path = f"{id}_backside.png"
+    #    crop_knife("back", save_path, id)
+    if weaponType == "Karambit" or weaponType == "Talon Knife":
+        save_path = f"{id}_backside.png" # iverser car le premier scree pris est le back side pour le talon
+        crop_talon("play", save_path, id)
         save_path = f"{id}_playside.png"
-        crop_knife("play", save_path, id)
-        save_path = f"{id}_backside.png"
-        crop_knife("back", save_path, id)
+        crop_talon("back", save_path, id)
     else:
         save_path = f"{id}_playside.png"
         crop("play", save_path, id)
@@ -122,9 +165,6 @@ def screen(url, id):
     time.sleep(1)
 
     final_image(id, data)
-
-    #-75 pour metre droit
-    #-370 pour back side
 
 def url_parse(url):
     splitedUrl = url.split("/")
@@ -144,16 +184,39 @@ def main(url, id):
 
         break
 
+"""#pistol test
 url = [
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M632011729189536458A7008814011D12295506487237922602",
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198251702978A7004230400D903048837917845911",
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M633138486229823008A7022654840D614823716239812366",
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198255004025A6980175061D9262440886311016501",
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M283004634227196325A9520265790D12538699143338037188",
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M1911141422672709113A16990064703D9387373730855472081",
-    #"steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M3167648443666738410A17182190108D336820863276418395",
-    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198822504212A18491077225D9693978678799666676"
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M567835434500285294A6890278457D7217761984680802747",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M633137629093035679A7009582189D7676003762701880389",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M1970780032104770464A14931685006D769557030645564088",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M567835434501727074A7013725658D7837503774794473695",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M629759929377113726A7013816152D315399352133020036",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M566709534597094873A7017038068D11963933693086198882",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M567835434504017014A7014852070D11576539041362223596",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M192910765526620051A7001529540D10278484185962303182",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M567838199905072112A7223698097D13881260557936212958",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M280752834392830333A9475872382D7791900731263490827",
         ]
+"""
+
+"""url = [
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198251702978A7004230400D903048837917845911",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S76561198254147490A6935333937D55310245988069934",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M628634029467888445A6995083397D6983640911094414019",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M573463979402187374A6847585353D9683911451363831966",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M1982033163480175927A14698000126D6939559547703154843",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M567835434498245724A7008651367D579312905880222669",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M1968528232297296432A14935104083D7812692350590599050",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M191784865621576470A7006284852D2460197461240251630",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M570089999715578734A7219360283D9578699314150025598",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M1969653972789156223A14926450430D3026986756764264255",
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M567835434496842504A7006292395D7809422409315546280",
+]
+"""
+
+url = [
+    "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M1975277764044161561A14702751549D7065844438316666997",
+    ]
 
 
 if __name__ == '__main__':
