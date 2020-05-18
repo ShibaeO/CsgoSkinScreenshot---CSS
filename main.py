a7e42ab4764165413fcf3f1ba1dee2d23e81365f
Antoine <42607264+ShibaeO@users.noreply.github.com>
2020-05-18 21:38:43 +0200

    fix knife, now suport all knife

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from config import huey
+import redis
 from flask import Flask, request
 from flask import jsonify, send_from_directory
-from worker import main
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-import time
-import redis
+
+from worker import main
+from config import huey
+
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
@@ -15,10 +16,10 @@ limiter = Limiter(app,
                   )
 limiter.init_app(app)
 
-@app.route("/inspect")
-@limiter.limit("5/minute")
-def index():
 
+@app.route("/inspect")
+@limiter.limit("25/minute")
+def index():
     if request.args.get("n"):
 
         a = request.args.get("n").split(" ")
@@ -38,11 +39,11 @@ def index():
             main(url, id)
 
             return jsonify(success=True,
-                            total_screenshot=int(r.get('total_scr')),
-                            queue_lenght=int(r.get('queue')),
-                            inspect_link=request.args.get("n"),
-                            image_link=f"/image/{id}.png",
-                            )
+                           total_screenshot=int(r.get('total_scr')),
+                           queue_lenght=int(r.get('queue')),
+                           inspect_link=request.args.get("n"),
+                           image_link=f"http://shibaeo.quetel.pro:5000/image/{id}.png",
+                           )
 
     return jsonify(success=False,
                    error="No inspect link provided",
@@ -53,16 +54,18 @@ def index():
 
 @app.route("/image/<path:filename>")
 def show_image(filename):
-    return send_from_directory("scr", filename, mimetype='image/jpeg',)
+    return send_from_directory("scr", filename, mimetype='image/jpeg', )
 
 
 @app.errorhandler(429)
-def ratelimit_handler():
+def ratelimit_handler(e):
     return jsonify(success=False,
                    error="Too many request calm down :)",
                    total_screenshot=int(r.get('total_scr')),
                    queue_lenght=int(r.get('queue')),
+                   var=f"{e}",
                    )
+
 
 if __name__ == "__main__":
     app.run(host="192.168.1.30")
