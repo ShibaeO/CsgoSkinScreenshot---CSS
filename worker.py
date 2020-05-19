d6d0ee7753b804aec52242345d6fbf49d9d7c3c0
Antoine <42607264+ShibaeO@users.noreply.github.com>
2020-05-19 20:12:36 +0200

    add different color based on float

--- worker.py
+++ worker.py
@@ -24,9 +24,6 @@ def get_window_info(window_name):
         w = rect[2] - x
         h = rect[3] - y
         if win32gui.GetWindowText(hwnd) == window_name:
-            # print("Window \"%s\":" % win32gui.GetWindowText(hwnd))
-            # print("\tLocation: (%d, %d)" % (x, y))
-            # print("\t    Size: (%d, %d)" % (w, h))
             location = {"x": x, "y": y}
             size = {"x": w, "y": h}
             window["location"] = location
@@ -48,69 +45,79 @@ def focus_window():
 def crop(mode, img, id):
     if mode == "play":
         im = Image.open(str(img))
-        border = (80, 230, 80, 180)  # left, up, right, bottom
+        border = (64, 230, 64, 180)  # left, up, right, bottom
         cropped = ImageOps.crop(im, border)
-        cropped.save(f"{id}_playside.png", "png")
+        cropped.save(f"{id}_cropped_playside.png", "png")
+        os.remove(f"{id}_playside.png")
     elif mode == "back":
         im = Image.open(str(img))
-        border = (80, 250, 80, 180)  # left, up, right, bottom
+        border = (64, 250, 64, 180)  # left, up, right, bottom
         cropped = ImageOps.crop(im, border)
-        cropped.save(f"{id}_backside.png", "png")
+        cropped.save(f"{id}_cropped_backside.png", "png")
+        os.remove(f"{id}_backside.png")
 
 
-def crop_knife(mode, img, id):
-    if mode == "play":
-        im = Image.open(str(img))
-        border = (330, 60, 180, 260)  # left, up, right, bottom
-        cropped = ImageOps.crop(im, border)
-        cropped.save(f"{id}_playside.png", "png")
-    elif mode == "back":
-        im = Image.open(str(img))
-        border = (180, 60, 330, 260)  # left, up, right, bottom
-        cropped = ImageOps.crop(im, border)
-        cropped.save(f"{id}_backside.png", "png")
+def crop_knife(mode, img, id, ktype):
+    if ktype == "all":
+        if mode == "play":
+            im = Image.open(str(img))
+            border = (64, 30, 64, 250)  # left, up, right, bottom
+            cropped = ImageOps.crop(im, border)
+            cropped.save(f"{id}_cropped_playside.png", "png")
+            os.remove(f"{id}_playside.png")
+        elif mode == "back":
+            im = Image.open(str(img))
+            border = (64, 30, 64, 250)  # left, up, right, bottom
+            cropped = ImageOps.crop(im, border)
+            cropped.save(f"{id}_cropped_backside.png", "png")
+            os.remove(f"{id}_backside.png")
+
+    if ktype == "tiger":
+        if mode == "play":
+            im = Image.open(str(img))
+            border = (3, 75, 125, 205)  # left, up, right, bottom
+            cropped = ImageOps.crop(im, border)
+            cropped.save(f"{id}_cropped_playside.png", "png")
+            os.remove(f"{id}_backside.png")
+        elif mode == "back":
+            im = Image.open(str(img))
+            border = (125, 75, 3, 205)  # left, up, right, bottom
+            cropped = ImageOps.crop(im, border)
+            cropped.save(f"{id}_cropped_backside.png", "png")
+            os.remove(f"{id}_playside.png")
+
 
 def crop_glove(mode, img, id):
     if mode == "play":
         im = Image.open(str(img))
-        border = (440, 50, 440, 350)  # left, up, right, bottom
+        border = (128, 50, 128, 350)  # left, up, right, bottom
         cropped = ImageOps.crop(im, border)
-        cropped.save(f"{id}_playside.png", "png")
+        cropped.save(f"{id}_cropped_playside.png", "png")
+        os.remove(f"{id}_playside.png")
     elif mode == "back":
         im = Image.open(str(img))
-        border = (440, 50, 440, 350)  # left, up, right, bottom
+        border = (64, 50, 64, 350)  # left, up, right, bottom
         cropped = ImageOps.crop(im, border)
-        cropped.save(f"{id}_backside.png", "png")
+        cropped.save(f"{id}_cropped_backside.png", "png")
+        os.remove(f"{id}_backside.png")
 
-"""def crop_talon(mode, img, id):
-    print(img)
-    if mode == "back":
-        img = Image.open(img)
-        img = img.rotate(-90)
-        border = (456, 340, 474, 140)  # left, up, right, bottom
-        im = ImageOps.crop(img, border)
-        im.save(f"{id}_backside.png", "png")
 
-    elif mode == "play":
-        img = Image.open(img)
-        img = img.rotate(90)
-        border = (456, 340, 474, 140)  # left, up, right, bottom
-        im = ImageOps.crop(img, border)
-        im.save(f"{id}_playside.png", "png")
-
-"""
 def crop_sticker_final(img, id, data):
     img = Image.open(img)
-    border = (400, 200, 400, 200)  # left, up, right, bottom
+    border = (112, 200, 112, 200)  # left, up, right, bottom
     im = ImageOps.crop(img, border)
     im.save(f"{id}_playside.png", "png")
 
     fullName = data["iteminfo"]["full_item_name"].replace("★", "")
-    sep = Image.new('RGB', (1760, 60), color=(24, 128, 122))
+
+    sep = Image.open("ressources/sep_template.png")
     d = ImageDraw.Draw(sep)
-    font = ImageFont.truetype("ressources/Bison.ttf", 25)
-    d.multiline_text((20, 14), f"{fullName}        Shibaeo <3", fill=(255, 255, 0), spacing=4, align="left", font=font)
-    sep.save('ressources/sep.png', "png")
+    floatFont = ImageFont.truetype("ressources/Futura.ttf", 30)
+    mainTextFont = ImageFont.truetype("ressources/FuturBold.ttf", 40)
+    d.multiline_text((173, 11), f"None", fill=(225, 256, 41), font=floatFont)
+    d.multiline_text((102, 52), f"None", fill=(225, 256, 41), font=floatFont)
+    d.multiline_text((650, 25), f"{fullName}", fill=(225, 256, 41), font=mainTextFont)
+    sep.save("ressources/sep.png")
 
     im1 = Image.open(f"{id}_playside.png")
     im3 = Image.open("ressources/sep.png")
@@ -125,25 +132,39 @@ def crop_sticker_final(img, id, data):
 
 def final_image(id, data):
     paintID = data["iteminfo"]["paintseed"]
-    floatVal = data["iteminfo"]["floatvalue"]
+    floatVal = float(data["iteminfo"]["floatvalue"])
     fullName = data["iteminfo"]["full_item_name"].replace("★", "")
-    sep = Image.new('RGB', (1760, 60), color=(24, 128, 122))
-    d = ImageDraw.Draw(sep)
-    font = ImageFont.truetype("ressources/Bison.ttf", 25)
-    d.multiline_text((20, 14), f"{fullName}        PaintID : {paintID}        float : {floatVal}        Shibaeo <3",
-                     fill=(255, 255, 0), spacing=4, align="left", font=font)
-    sep.save('ressources/sep.png', "png")
 
-    im1 = Image.open(f"{id}_playside.png")
-    im2 = Image.open(f"{id}_backside.png")
+    if floatVal > 0.45:
+        floatColor = (255, 0, 0)
+    elif floatVal > 0.38 and floatVal < 0.45:
+        floatColor = (255, 127, 80)
+    elif floatVal > 0.15 and floatVal < 0.38:
+        floatColor = (255, 165, 0)
+    elif floatVal > 0.07 and floatVal < 0.15:
+        floatColor = (64, 224, 208)
+    elif floatVal > 0.0 and floatVal < 0.07:
+        floatColor = (173, 255, 47)
+
+    sep = Image.open("ressources/sep_template.png")
+    d = ImageDraw.Draw(sep)
+    floatFont = ImageFont.truetype("ressources/Futura.ttf", 30)
+    mainTextFont = ImageFont.truetype("ressources/FuturBold.ttf", 40)
+    d.multiline_text((173, 11), f"{paintID}", fill=(225, 256, 41), font=floatFont)
+    d.multiline_text((102, 52), f"{floatVal}", fill=floatColor, font=floatFont)
+    d.multiline_text((650, 25), f"{fullName}", fill=(225, 256, 41), font=mainTextFont)
+    sep.save("ressources/sep.png")
+
+    im1 = Image.open(f"{id}_cropped_playside.png")
+    im2 = Image.open(f"{id}_cropped_backside.png")
     im3 = Image.open("ressources/sep.png")
     dst = Image.new('RGB', (im1.width, im1.height + im3.height + im2.height))
     dst.paste(im1, (0, 0))
     dst.paste(im3, (0, im2.height))
     dst.paste(im2, (0, im2.height + im3.height))
     dst.save(f"scr/{id}.png", "png")
-    os.remove(f"{id}_playside.png")
-    os.remove(f"{id}_backside.png")
+    os.remove(f"{id}_cropped_playside.png")
+    os.remove(f"{id}_cropped_backside.png")
     os.remove(f"ressources/sep.png")
 
 
@@ -168,20 +189,19 @@ def screen(url, id):
     time.sleep(1)
     ImageGrab.grab().save(save_path)
 
-    """    if weaponType == "Karambit" or weaponType == "Talon Knife":
-            save_path = f"{id}_backside.png"
-            crop_talon("back", save_path, id)
-            save_path = f"{id}_playside.png"
-            crop_talon("play", save_path, id)
-            time.sleep(1)
-            final_image(id, data)
-    """
-
-    if weaponType == "Bayonet" or weaponType == "Karambit" or weaponType == "Shadow Daggers" or "Knife" in weaponType or weaponType == "Karambit":
-        save_path = f"{id}_playside.png"
-        crop_knife("play", save_path, id)
+    if weaponType == "Karambit" or weaponType == "Talon Knife":
         save_path = f"{id}_backside.png"
-        crop_knife("back", save_path, id)
+        crop_knife("play", save_path, id, ktype="tiger")
+        save_path = f"{id}_playside.png"
+        crop_knife("back", save_path, id, ktype="tiger")
+        time.sleep(1)
+        final_image(id, data)
+
+    elif weaponType == "Bayonet" or weaponType == "Shadow Daggers" or "M9" in weaponType or "Knife" in weaponType:
+        save_path = f"{id}_playside.png"
+        crop_knife("play", save_path, id, ktype="all")
+        save_path = f"{id}_backside.png"
+        crop_knife("back", save_path, id, ktype="all")
         time.sleep(1)
         final_image(id, data)
 
@@ -212,17 +232,17 @@ def url_parse(url):
     parsedUrl = splitedUrl[5].replace("+csgo_econ_action_preview%20", "")
     return parsedUrl
 
+
 @huey.task()
 def main(url, id):
-
     global super_Var_lol
 
     super_Var_lol = get_window_info("CS:GO MIGI")
+    print(f"         ")
     print(f"         [+] {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} Got CSGO Window")
     print(f"         [+] {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} -----> Taking screenshot of item id : {id}")
     print(f"         [+] {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} Queue size : {r.get('queue')}\n")
     while True:
-
         focus_window()
         screen(url, id)
 
@@ -231,12 +251,12 @@ def main(url, id):
         break
 
 
-#place url here :
+# place url here :
 """url = [
     "steam://rungame/730/76561202255233023/+csgo_econ_action_preview S76561198106428990A15181179134D4911215139517499306",
     "steam://rungame/730/76561202255233023/+csgo_econ_action_preview S76561198213656201A7496627946D4911215139517499306",
 ]"""
-#url_end
+# url_end
 
 
 """if __name__ == '__main__':
